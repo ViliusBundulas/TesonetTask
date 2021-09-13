3d079cf28fbe1eccc0298ee038c6fdea385e85c5
ViliusBundulas <vilius.bundulas@gmail.com>
2021-09-13 22:55:02 +0300

    Initial Commit

---
+++ TesonetTask/TesonetTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  TesonetTaskApp.swift
+//  TesonetTask
+//
+//  Created by Vilius Bundulas on 2021-09-13.
+//
+
+import SwiftUI
+
+@main
+struct TesonetTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
